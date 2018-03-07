31b0854fd82a7d3c703075702fa6d9b330294c4f
Andrew Matthews <matthews.andrew@gmail.com>
2018-03-08 05:43:04 +1100

    enhanced backup on bootstrap

--- bootstrap.fish
+++ bootstrap.fish
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
 
+function backup_home_links -d "move everything off into a sub directory to keep ~ clean"
+  set -l p ~/.fishdots_backups/backup_(date +%Y%m%d-%H)
+  mkdir -p $p
+  for i in (find $HOME -maxdepth 1 -type l -name '.*' -not -name '.fishdots')
+    mv $i $p
+  end
+end
+
+backup_home_links
+
 # assume that we are in the directory that needs to be linked
 # link it through to .config/fish/config.fish
 
@@ -28,13 +38,12 @@ function to_dotform -d "transform a name from symlink form into dotfile form"
 end
 
 function make_symlink -d "transforms a symlink file or directory into a dotfile in the $HOME directory"
-    set origin $argv[1]
-    set dotform "$HOME/."(basename -s .symlink $origin)
-    if test -f "$dotform"
-        mv -f $dotform $dotform.backup
-    end 
-    ln -fs "$origin" "$dotform"
-    #echo "linking $origin to $dotform"
+  set origin $argv[1]
+  set dotform "$HOME/."(basename -s .symlink $origin)
+# if test -f "$dotform"
+# mv -f $dotform $dotform.backup
+# end 
+  ln -fs "$origin" "$dotform"
 end
 
 for l in (find . -name "*.symlink")
