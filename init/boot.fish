6a5072bbc757b57901a73a886718a38ce54e5c5e
Andrew Matthews <matthews.andrew@gmail.com>
2018-03-12 08:56:41 +1100

    incremental boot process pivoted

--- init/boot.fish
+++ init/boot.fish
@@ -1,47 +1,66 @@
 #!/usr/bin/env fish
 
 # this is the one env var that needs to be set up prior to the boot process
-set -U DOTFILES (realpath ~/.fishdots)
+set -U FISHDOTS (realpath ~/.fishdots)
 set -U FISHDOTS_CONFIG ~/.config/fishdots
 set -U FISHDOTS_PLUGINS_HOME "$FISHDOTS_CONFIG/plugins"
-
 set -x QUIET_FISHDOTS_BOOT_LOGGING true
+set -g FD_MAX_RUN_LEVEL 5
 
-# this will have been invoked from .config/fish/config.fish, which will have set $DOTFILES
+if test $QUIET_FISHDOTS_BOOT_LOGGING  != true
+    echo "Welcome to fishdots"
+    echo 
+end
 
-function log
-    if test $QUIET_FISHDOTS_BOOT_LOGGING  != true
-        set_color brred
-        echo -n " ⇒ "
+function colour_print -a colour message
+        set_color $colour
+        printf "%s" $message
         set_color normal
-        echo $argv[1]
+end
+
+function fecho -a icon icon_colour msg  -d "display text with  (prefix, colour) msg"
+    if test $QUIET_FISHDOTS_BOOT_LOGGING  != true
+        colour_print $icon_colour $icon
+        colour_print normal $msg
+        echo
     end
 end
 
-function welcome -d "display a welcome banner"
-    log "Welcome to fishdots"
-    echo ""
-end
+function boot    -a message; fecho "[BOOT] "    bryellow "$message"; end
+function ok      -a message; fecho "[OK] "      green    "$message"; end
+function running -a message; fecho "    => "    cyan     "$message"; end
+function action  -a message; fecho "[action] "  yellow   "$message"; end
+function warn    -a message; fecho "[warning] " yellow   "$message"; end
+function error   -a message; fecho "[error] "   red      "$message"; end
 
-welcome
-
+boot "Booting Fishdots"
 # run each of the init scripts in each init level in turn
-for level in (seq 0 2)
-    for script in (find "$DOTFILES/init/rc"$level".d/" -name "*.fish")
-        log (basename $script)        
-        source $script
+for level in (seq 0 $FD_MAX_RUN_LEVEL)
+    if test -e "$FISHDOTS/init/rc"$level".d/"
+        ok "Level: $level"
+        for script in (find "$FISHDOTS/init/rc"$level".d/" -name "*.fish")
+            running (basename $script)        
+            source $script
+        end
     end
 end
 
-# run each of the init scripts in each of the plugins
-# rename a plugin to have a leading underscore to ignore it
-for plugin in (find $FISHDOTS_PLUGINS_HOME -maxdepth 1 -mindepth 1 -type d -not -iname "_*")
-    log (basename $plugin)
-    for level in (seq 0 2)
+# boot to each level on each of the plugins before commencing the next run level
+# that way, if one plugin depends on functions or environment variables established
+# elsewhere, they can be confident to have them present when they make their own definitions 
+echo
+boot "Loading Fishdots Plugins"
+for level in (seq 0 $FD_MAX_RUN_LEVEL)
+    ok "Level: $level"
+    for plugin in (find $FISHDOTS_PLUGINS_HOME -maxdepth 1 -mindepth 1 -type d -not -iname "_*")
         if test -e $plugin/init/rc"$level".d/
+            fecho "  => " blue (basename $plugin)
             for script in (find "$plugin/init/rc"$level".d/" -name "*.fish")
+                running (basename $script)        
                 source $script
             end
         end
     end
 end
+# IDEA: rename a plugin to have a leading underscore to ignore it
+
