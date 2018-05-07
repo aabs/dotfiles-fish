d3a218e3bcef2d6f3298f24640f97bcf19e76561
Andrew Matthews <matthews.andrew@gmail.com>
2018-05-08 09:07:52 +1000

    ditto

--- install.fish
+++ install.fish
@@ -20,3 +20,20 @@
 # 4. Establish a first generation `$HOME/.config/fishdots/gens/gen1` that holds
 #    links to the scripts established by default by fishdots.
 
+
+# 1. Create a download area, into which the release will be decompressed.
+set FD_HOME $HOME/.config/fishdots
+mkdir -p $FD_HOME
+cd $FD_HOME
+set -l rel ()
+curl -L https://github.com/aabs/fishdots/releases/download/v0.0.1_alpha/fishdots-0.0.1.tar.gz | tar xzvf -
+
+
+# 2. Establish a dotfiles management area called `$HOME/.config/fishdots/gens` 
+#    where all scripts and plugins will be stored
+
+# 3. Replace all pre-existing scripts with indirect symlinks that 
+#    point to the original files stored in `$HOME/.config/fishdots/gens/original`.
+
+# 4. Establish a first generation `$HOME/.config/fishdots/gens/gen1` that holds
+#    links to the scripts established by default by fishdots.
