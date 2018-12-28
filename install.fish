f76a5130ad7171d0ed619c3c5530bf44070fc3e2
andrew matthews <matthews.andrew@gmail.com>
2018-12-28 16:16:03 +1100

    removed crap from tools dir

--- install.fish
+++ install.fish
@@ -22,7 +22,8 @@
 
 
 # 1. Create a download area, into which the release will be decompressed.
-set -U FISHDOTS $HOME/.config/fishdots/home
+set -l latest_release (curl https://raw.githubusercontent.com/aabs/fishdots/feature/nix-bootstrap/latest_release.txt)
+set -U FISHDOTS "$HOME/.config/fishdots/home/fishdots-$latest_release"
 
 if test -e $FISHDOTS
     echo "Fishdots already installed. Aborting web installer."
@@ -30,14 +31,14 @@ if test -e $FISHDOTS
 end
 
 mkdir -p $FISHDOTS
-cd $FISHDOTS
-set -l latest_release (curl https://raw.githubusercontent.com/aabs/fishdots/feature/nix-bootstrap/latest_release.txt)
-curl -L https://github.com/aabs/fishdots/releases/download/{$latest_release}/fishdots-{$latest_release}.tar.gz | tar xzvf -
+cd $FISHDOTS/..
+curl -L https://github.com/aabs/fishdots/archive/v{$latest_release}.tar.gz | tar xzvf -
 
 
 # 2. Establish a dotfiles management area called `$HOME/.config/fishdots/gens` 
 #    where all scripts and plugins will be stored
-mkdir -p $FISHDOTS/../gens/original
+mkdir -p $FISHDOTS/../../gens/original
+mkdir -p $FISHDOTS/../../plugins
 
 # 3. Replace all pre-existing scripts with indirect symlinks that 
 #    point to the original files stored in `$HOME/.config/fishdots/gens/original`.
@@ -47,4 +48,4 @@ mkdir -p $FISHDOTS/../gens/original
 
 # 5. setup invocation script for fishdots loader
 mkdir -p $HOME/.config/fish
-echo -e "#!/usr/bin/env fish\n\nsource $HOME/.config/fishdots/home/init/boot.fish" >$HOME/.config/fish/config.init
+echo -e "#!/usr/bin/env fish\n\nsource $FISHDOTS/init/boot.fish" >$HOME/.config/fish/config.fish
