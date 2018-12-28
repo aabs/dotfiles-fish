9f78ea295229471c7e219032f2cb7996a27a808b
andrew matthews <matthews.andrew@gmail.com>
2018-12-28 16:30:25 +1100

    corrected error in setting FISHDOTS env var

--- init/boot.fish
+++ init/boot.fish
@@ -1,11 +1,12 @@
 #!/usr/bin/env fish
 
 # this is the one env var that needs to be set up prior to the boot process
-set -U FISHDOTS (realpath ~/.config/fishdots/home)
-set -U FISHDOTS_CONFIG ~/.config/fishdots/config
+# fishdots should have been persistently set during install.
+#set -U FISHDOTS (realpath ~/.config/fishdots/home)
+set -U FISHDOTS_CONFIG $FISHDOTS/config
 set -U FISHDOTS_PLUGINS_HOME ~/.config/fishdots/plugins
 
-set -x QUIET_FISHDOTS_BOOT_LOGGING true
+set -x QUIET_FISHDOTS_BOOT_LOGGING false
 set -g FD_MAX_RUN_LEVEL 4
 
 if test $QUIET_FISHDOTS_BOOT_LOGGING  != true
