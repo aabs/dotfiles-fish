39696eefb761b27466f45b24f0e3c6b72a4e6515
andrew matthews <matthews.andrew@gmail.com>
2018-12-29 12:28:13 +1100

    test scripts

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
 release_tag=0.0.5
 release_contents=bin init editors tools bootstrap.fish indirection.fish install.fish known-plugins.dat profile.symlink README.org
+
+.PHONY: release
+
 release:
 	echo "$(release_tag)" > latest_release.txt
 	git add -A .
 	git commit -m "release $(release_tag) checkin"
 	git tag -f $(release_tag)
 	git push --all --tags --atomic 
+
+prep:
+	mkdir -p ~/tmp/fishdots_test
+
+
+clean:
+	@rm -rf ~/.config/fishdots
+	@rm -f ~/.config/fish/config.*
+	@rm -rf ~/tmp/fishdots_test
--- testing.fish
+++ testing.fish
@@ -6,32 +6,48 @@
 
 function fixture -d "run all the tests"
     set test_path (setup)
+    set -g errors ""
     run_tests $test_path
+    if test -n $errors
+      echo "Errors:"
+      printf $errors
+      set -g errors ""
+    else
+      echo "all tests passed"
+    end
     teardown $test_path
 end
 
 function run_tests -a test_path -d "description"
-    
+   test_existence_of_test_files $test_path 
 end
 
 function setup -d "set everything up"
-    set path (create_test_folder)
-    echo $path
+    set test_path (create_test_folder)
+    echo $test_path
 end
 
-function teardown -a path -d "clean up"
-    remove_home_folder_structure $path
+function teardown -a test_path -d "clean up"
+    remove_home_folder_structure $test_path
 end
+
+function on_fail --on-event failed_unit_test -a msg
+  set -g errors "$errors \n  - $msg"
+end
+
+function fail -a msg
+  emit failed_unit_test $msg
+end
+
 
 function create_test_folder -d "creates a temporary directory for testing"
-    set path ( get_test_folder (get_base_path) )
-    mkdir -p $path
-    setup_home_folder_structure $path
-    echo $path
+    set test_path ( get_test_folder (get_base_path) )
+    setup_home_folder_structure $test_path
+    echo $test_path
 end
 
 function get_base_path -d "find a place for the test directory to go"
-    echo "/tmp/fishdots_test"
+    echo "$HOME/tmp/fishdots_test"
 end
 
 function get_test_folder -a base_path -d "generates a name for a test folder and creates it"
@@ -40,12 +56,13 @@ function get_test_folder -a base_path -d "generates a name for a test folder and
     echo $p
 end
 
-function setup_home_folder_structure -a path -d "places sample home folder files and folders into the test folder"
-    tar -xzvf test_home_folder.tgz -C $path
+function setup_home_folder_structure -a test_path -d "places sample home folder files and folders into the test folder"
+    mkdir -p $test_path
+    tar xzf test_home_folder.tgz -C $test_path
 end
 
-function remove_home_folder_structure -a path -d "description"
-    rm -rf $path
+function remove_home_folder_structure -a test_path -d "description"
+    rm -rf $test_path
 end
 
 function is_file_under_control -a file_name -d "determines whether a file has been placed under indirection control"
@@ -56,3 +73,17 @@ function get_gen_of_file -a file_name -d "traces the location of a file and conf
     
 end
 
+#  UNIT TESTS
+###################################
+
+function test_existence_of_test_files -a test_path 
+  echo "test_existence_of_test_files "
+  if not test -e $test_path/.local
+    fail "test files were not present"
+  end
+end
+
+function test_creation_of_generation_structure -a test_path
+end
+
+abbr --add test 'make clean; source testing.fish; fixture'
