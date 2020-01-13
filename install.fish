d2233fe37e674745794b14cc692837485d582117
Andrew Matthews <matthews.andrew@gmail.com>
2020-01-13 11:00:19 +1100

    wip

--- install.fish
+++ install.fish
@@ -41,25 +41,25 @@ if test -e $FISHDOTS
     exit 1
 end
 
-exit 1
+# return 1
 
-mkdir -p $FISHDOTS
-cd $FISHDOTS/..
-curl -L https://github.com/aabs/fishdots/archive/v{$latest_release}.tar.gz | tar xzf -
+# mkdir -p $FISHDOTS
+# cd $FISHDOTS/..
+# curl -L https://github.com/aabs/fishdots/archive/v{$latest_release}.tar.gz | tar xzf -
 
-# 2. Establish a dotfiles management area called `$HOME/.config/fishdots/gens`
-#    where all scripts and plugins will be stored
-mkdir -p $FISHDOTS/../../plugins
+# # 2. Establish a dotfiles management area called `$HOME/.config/fishdots/gens`
+# #    where all scripts and plugins will be stored
+# mkdir -p $FISHDOTS/../../plugins
 
-set -U GEN_ROOT "$FISHDOTS/../../gens"
-# 3. Replace all pre-existing scripts with indirect symlinks that
-#    point to the original files stored in `$HOME/.config/fishdots/gens/original`.
-source $FISHDOTS/indirection.fish
-install_for_first_time $FISHDOTS_INSTALL_PATH
+# set -U GEN_ROOT "$FISHDOTS/../../gens"
+# # 3. Replace all pre-existing scripts with indirect symlinks that
+# #    point to the original files stored in `$HOME/.config/fishdots/gens/original`.
+# source $FISHDOTS/indirection.fish
+# install_for_first_time $FISHDOTS_INSTALL_PATH
 
-# 4. Establish a first generation `$HOME/.config/fishdots/gens/gen1` that holds
-#    links to the scripts established by default by fishdots.
+# # 4. Establish a first generation `$HOME/.config/fishdots/gens/gen1` that holds
+# #    links to the scripts established by default by fishdots.
 
-# 5. setup invocation script for fishdots loader
-mkdir -p $HOME/.config/fish
-echo -e "#!/usr/bin/env fish\n\nsource $FISHDOTS/init/boot.fish" >$HOME/.config/fish/config.fish
+# # 5. setup invocation script for fishdots loader
+# mkdir -p $HOME/.config/fish
+# echo -e "#!/usr/bin/env fish\n\nsource $FISHDOTS/init/boot.fish" >$HOME/.config/fish/config.fish
