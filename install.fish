350ce5e408137afe4706472463992df40308f45b
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-06 08:31:16 +1000

    installer cleanup

--- install.fish
+++ install.fish
@@ -23,7 +23,7 @@
 # One liner install script
 # ========================
 
-# curl -skq https://raw.githubusercontent.com/aabs/fishdots/master/install.fish | fish
+# curl -skq bit.ly/fishdots_install | fish
 
 # 1. Create a download area, into which the release will be decompressed.
 set -l latest_release (curl -skq https://raw.githubusercontent.com/aabs/fishdots/master/latest-release.txt)
@@ -45,20 +45,3 @@ end
 mkdir -p $FISHDOTS
 cd $FISHDOTS/..
 curl -L https://github.com/aabs/fishdots/archive/v{$latest_release}.tar.gz | tar xzf -
-
-# # 2. Establish a dotfiles management area called `$HOME/.config/fishdots/gens`
-# #    where all scripts and plugins will be stored
-# mkdir -p $FISHDOTS/../../plugins
-
-# set -U GEN_ROOT "$FISHDOTS/../../gens"
-# # 3. Replace all pre-existing scripts with indirect symlinks that
-# #    point to the original files stored in `$HOME/.config/fishdots/gens/original`.
-# source $FISHDOTS/indirection.fish
-# install_for_first_time $FISHDOTS_INSTALL_PATH
-
-# # 4. Establish a first generation `$HOME/.config/fishdots/gens/gen1` that holds
-# #    links to the scripts established by default by fishdots.
-
-# # 5. setup invocation script for fishdots loader
-# mkdir -p $HOME/.config/fish
-# echo -e "#!/usr/bin/env fish\n\nsource $FISHDOTS/init/boot.fish" >$HOME/.config/fish/config.fish
