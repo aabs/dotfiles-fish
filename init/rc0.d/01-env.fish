ef12825e16bcb6a1baee58e953420591e4e0aeca
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-06 10:58:03 +1000

    remove spurious personal configurations

--- init/rc0.d/01-env.fish
+++ init/rc0.d/01-env.fish
@@ -1,18 +1,3 @@
 #!/usr/bin/env fish
 
-set -x PATH "$HOME/go/bin" "/home/linuxbrew/.linuxbrew/bin" "$HOME/dev/tools/jena/bin" "$HOME/bin" "$HOME/.fishdots/bin" $PATH
-set -x EDITOR nvim
-set -x FISHDOTS_PLUGINS_HOME $HOME/.config/fishdots/plugins
-
-# too many other things depend on this for their rc0.d loading to put this in a plugin
-if test -e /mnt/d/
-    set -x SYNC_HOME /mnt/d/Synchronised
-else if test -e /d/
-    set -x SYNC_HOME /d/Synchronised
-else if test -e /media/aabs/DATA/
-    set -x SYNC_HOME /media/aabs/DATA/Synchronised
-else
-    set_color red
-    echo "[ERROR] unable to find the Sync directory"
-    set_color normal
-end
+set -x PATH "$HOME/bin" $PATH
